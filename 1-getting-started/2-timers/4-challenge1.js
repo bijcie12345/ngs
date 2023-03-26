b2b74a0cf997cb2c244030764b9f7a734ba480fe
root <root@DESKTOP-A2UIREC.localdomain>
2023-03-26 16:09:07 +0200

    2-timers_4-challenge1

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,20 @@
-const theOneFunc = () => {};
+const theOneFunc = arg => {
+    console.log("Hello after " + arg + " seconds")
+};
 
-setTimeout(theOneFunc, 4 * 1000);
+const arg1 = 4;
+
+setTimeout(theOneFunc, arg1 * 1000, arg1);
+setTimeout(theOneFunc, arg1 * 2 * 1000, arg1 * 2);
 
 // Hello after 4 seconds
 
 // Hello after 8 seconds
 
 // You can define only ONE function
+
+//  Start point
+// 4 sec |         |  
+// 8 sec |                     |
+//   sec  1  2  3  4  5  6  7  8 
+// program called function with 8 sec argument 4 sec after the function with 4s argument
