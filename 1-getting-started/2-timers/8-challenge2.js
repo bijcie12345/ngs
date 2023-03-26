7f679be0ad63b9c77b14f0ee28f180b7e493168f
root <root@DESKTOP-A2UIREC.localdomain>
2023-03-26 16:28:07 +0200

    2-timers_8-challenge2

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -2,4 +2,18 @@
 // Every second
 // And stop after 5 times
 
+const { exit } = require("process");
+
 // After 5 times. Print "Done" and let Node exit.
+
+let counter = 0;
+
+setInterval(() =>{
+    console.log("Hello World");
+    counter +=1;
+
+    if (counter == 5){
+        console.log("Done");
+        exit(1);
+    }
+}, 1000);
